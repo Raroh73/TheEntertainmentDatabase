9e311be0b3cda6ebfdd12f70bf925e0f54192e51
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2025-11-07 17:01:29 +0100

    Use exact versions

--- Package.swift
+++ Package.swift
@@ -8,9 +8,9 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
+        .package(url: "https://github.com/vapor/vapor.git", exact: "4.119.0"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", exact: "2.88.0"),
     ],
     targets: [
         .executableTarget(
